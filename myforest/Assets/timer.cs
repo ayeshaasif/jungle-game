a1133729c7dbd626d440c0947873ae1dbdd76525
ayeshaasif <ayeshaa666@gmail.com>
2015-05-18 14:31:59 +0500

    skybox and Firstperson controller is added

---
+++ myforest/Assets/timer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+    float  myTimer = 6.67;
+	// Update is called once per frame
+	void Update () {
+        if (myTimer > 0)
+        {
+            myTimer -= Time.deltaTime;
+        }
+        if (myTimer <= 0)
+        {
+            Debug.Log("GAME OVER");
+        }
+	}
+}
