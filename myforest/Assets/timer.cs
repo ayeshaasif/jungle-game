e1339a8618e1fb39e7789aa56f1b5be7121e7351
ayeshaasif <ayeshaa666@gmail.com>
2015-05-31 01:06:13 +0500

    reduced batches from 2088 to 1767

--- myforest/Assets/timer.cs
+++ myforest/Assets/timer.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class timer : MonoBehaviour
 {
-
+//public GUIText guiscore ;
+//public GUIText guitimer;
+//	public float time;
 	// Use this for initialization
   public float myTimer = 300.0f;
 	// Update is called once per frame
+	void start(){
+		    //guiscore.text = "Score: 0";
+		    //guiscore.font.material.color = Color.black;
+	}
 	void Update () 
     {
+
         if (myTimer > 0)
         {
             myTimer -= Time.deltaTime;
+//			float minutes = myTimer / 60; float minutes1 = 0;
+//			float seconds = myTimer % 60; float seconds1 = 0;
+//			float fraction = (myTimer * 100) % 100; float fraction1 = 0;
+//			guitimer.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, fraction);
+//			guitimer.font.material.color = Color.black;
+
         }
         if (myTimer <= 0)
         {
